7375b58ff275d9d56e7f9e59ec0881663865afb4
Élie Meignan <contact@eliepse.fr>
2023-11-14 21:01:40 +0100

    Add tests

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,12 +1,20 @@
-import {test, expect} from "bun:test";
+import {describe, expect,  test} from "bun:test";
 import {intToAlpha} from "../src/utils";
 
-test("inToAlpha", () => {
-    expect(intToAlpha(0)).toBe("a");
-    expect(intToAlpha(25)).toBe("z");
-    expect(intToAlpha(26)).toBe("A");
-    expect(intToAlpha(52)).toBe("aa");
-    expect(intToAlpha(2704)).toBe("Za");
-    expect(intToAlpha(8934)).toBe("cpQ");
+describe("intToAlpha", () => {
+    test("only accepts integers", () => {
+        expect(() => intToAlpha(42)).not.toThrow();
+        expect(() => intToAlpha(0.3)).toThrow();
+        expect(() => intToAlpha(Number.NaN)).toThrow();
+    });
 
-})
+    test("convert decimal to base 52 ([a-zA-Z])", () => {
+        expect(intToAlpha(0)).toBe("a");
+        expect(intToAlpha(25)).toBe("z");
+        expect(intToAlpha(26)).toBe("A");
+        expect(intToAlpha(52)).toBe("aa");
+        expect(intToAlpha(2704)).toBe("Za");
+        expect(intToAlpha(8934)).toBe("cpQ");
+    });
+});
+
